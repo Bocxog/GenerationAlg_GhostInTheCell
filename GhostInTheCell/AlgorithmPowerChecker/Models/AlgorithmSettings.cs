dd89a35e0854c0558005fb8f36f51a3e350520ad
DESKTOP-7A40RVD\Anton <selmernoid@mail.ru>
2019-04-09 00:40:38 +0300

    AlgorithmPowerChecker

---
+++ GhostInTheCell/AlgorithmPowerChecker/Models/AlgorithmSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPowerChecker {
+    public class AlgorithmSettings {
+        public string PathToExecutable { get; set; }
+    }
+}
---
+++ GhostInTheCell/AlgorithmPowerChecker/Models/SingleFightResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPowerChecker {
+    public enum SingleFightResult {
+        FirstWin   = 1,
+        SecondLose = 1,
+
+        SecondWin = 2,
+        FirstLost = 2,
+
+        Draw = 0
+    }
+}
